e4e66fab7623542b07db297e81bfe4c78539c101
ShradhaGupta26 <88206733+ShradhaGupta26@users.noreply.github.com>
2023-08-01 14:24:17 +0530

    Add files via upload

---
+++ vpc-vr/output.tf
@@ -0,0 +1,27 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "private_subnet_id_1" {
+  value = aws_subnet.private-1.id
+}
+
+output "private_subnet_id_2" {
+  value = aws_subnet.private-2.id
+}
+
+output "public_subnet_id_1" {
+  value = aws_subnet.public-1.id
+}
+
+output "public_subnet_id_2" {
+  value = aws_subnet.public-2.id
+}
+
+output "security_group-id" {
+  value = aws_security_group.allow_tls.id
+}
+
+output "keypair_name" {
+  value = aws_key_pair.my_keypair.key_name
+}
